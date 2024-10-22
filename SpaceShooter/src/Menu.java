0750e0b7875af51c9c38bf203ad8e46273bfaf1f
EliaFedriga <elia99ita@gmail.com>
2024-10-22 20:33:50 +0200

    Modificate le classi Menu e PlayerShip

--- SpaceShooter/src/Menu.java
+++ SpaceShooter/src/Menu.java
@@ -1,2 +1,10 @@
+import java.awt.*;
+
 public class Menu {
+
+    //menu di gioco
+    void draw(Graphics g) {}
+
+    //gestire input utente
+    void handleInput(){}
 }
--- SpaceShooter/src/PlayerShip.java
+++ SpaceShooter/src/PlayerShip.java
@@ -1,2 +1,31 @@
+import java.awt.*;
+
 public class PlayerShip {
+
+    private int x;
+    private int y;
+    private int width;
+    private int height;
+    private int lives;
+
+    //costruttore
+    PlayerShip(int startX, int startY) {}
+
+    //per il movimento
+    void move(int dx, int dy){}
+
+    //per istanziare un nuovo proiettile
+    Bullet shoot(){}
+
+    //riduce di 1 il numero di vite
+    void loseLife(){}
+
+    //ritorna il numero di vite
+    int getLives(){
+        return lives;
+    }
+
+    //disegna la navicella
+    void draw(Graphics g){}
+
 }
