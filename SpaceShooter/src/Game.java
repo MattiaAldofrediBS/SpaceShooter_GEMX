c2dbbaa640a120beff0216721c7de09b4d310cf6
MattiaAldofrediBS <m.aldofredi@studenti.unibs.it>
2024-10-22 18:08:45 +0200

    Create Variabili e costruttore classe game

--- SpaceShooter/src/Game.java
+++ SpaceShooter/src/Game.java
@@ -1,2 +1,27 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Timer;
+
 public class Game {
+    private PlayerShip player;
+    private ArrayList<Bullet> bullets;         // Active Bullets
+    private ArrayList<Asteroid> asteroids;     // Active Asteroids
+    private Timer timer;                        // Timer for updates
+    private Leaderboard leaderboard;            // Leaderbords ref
+    private boolean gameRunning;                // True or False, status of the game
+
+    //Window size
+    private final int WIDTH = 800;             // Window width
+    private final int HEIGHT = 600;            // Window Height
+
+    // Costruttore
+    public Game() {
+        // player = new PlayerShip(WIDTH / 2, HEIGHT - 50);
+        bullets = new ArrayList<>();
+        asteroids = new ArrayList<>();
+        leaderboard = new Leaderboard();
+        gameRunning = true;
+    }
 }
