eaa20b4069c68bcf61e275739490f3ef57460e1c
vinagiuls <g.vinati001@studenti.unibs.it>
2024-11-04 12:19:47 +0100

    Modifiche Asteroid Bullet

--- SpaceShooter/src/Asteroid.java
+++ SpaceShooter/src/Asteroid.java
@@ -1,2 +1,72 @@
 public class Asteroid {
+
+    //Variabili di istanza
+    private int x;
+    private int y;
+    private int speed;
+    private int size;
+    private boolean isActive;
+
+    //Costruttore
+    public Asteroid(int startX, int startY) {
+        this.x = startX;
+        this.y = startY;
+        this.speed = 10;
+        this.size = 5;
+        this.isActive = true;
+    }
+
+    //isActive() che restituisce isActive
+    public boolean isActive() {
+        return isActive;
+    }
+
+    //deactivate() che setta isActive a false
+    public void deactivate() {
+        this.isActive = false;
+    }
+
+    //Getter e Setter
+    public int getX() {
+        return x;
+    }
+
+    public int setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int setY(int y) {
+        this.y = y;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public int setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int setSize(int size) {
+        this.size = size;
+    }
+
+    @Override
+    public String toString() {
+        return "Asteroid{" +
+                "x=" + x +
+                ", y=" + y +
+                ", speed=" + speed +
+                ", size=" + size +
+                ", isActive=" + isActive +
+                "}";
+    }
 }
--- SpaceShooter/src/Bullet.java
+++ SpaceShooter/src/Bullet.java
@@ -1,2 +1,71 @@
 public class Bullet {
+
+    //Variabili di istanza
+    private int x;
+    private int y;
+    private int speed;
+    private int width;
+    private int height;
+    private boolean isActive;
+
+    //Costruttore
+    public Bullet(int startX, int startY) {
+        this.x = startX;
+        this.Y = startY;
+        this.speed = 5;
+        this.width = 10;
+        this.height = 20;
+        this.isActive = true;
+    }
+
+    //isActive() che restituisce isActive
+    public boolean isActive() {
+        return isActive;
+    }
+
+    //deactivate() che setta isActive a false
+    public void deactivate() {
+        this.isActive = false;
+    }
+
+    //Getter e Setter
+    public int getX() {
+        return x;
+    }
+
+    public int setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int setY(int y) {
+        this.y = y;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public int setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int setHeight(int height) {
+        this.height = height;
+    }
 }
